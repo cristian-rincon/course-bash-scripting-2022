428200309ae4515fd2bf0cecc30edce44b465c86
crrincon <cristian.rincon@mercadolibre.com.co>
2022-02-08 18:21:15 -0500

    :sparkles: variable scopes class

--- 1_intro/1_first_script.sh
+++ 1_intro/1_first_script.sh
@@ -2,4 +2,12 @@
 # Program:
 #   This program allows you to perform utilitary operations for PostgreSQL.
 
+PROGRAM_NAME="PostgreSQL utilitary operations"
+
 echo "This program allows you to perform utilitary operations for PostgreSQL."
+echo -e "You should input the operation you want to perform.\n"
+
+# Export variable to be used in the next script
+export PROGRAM_NAME
+# Run the next script
+$(pwd)/1_intro/2_variables.sh
