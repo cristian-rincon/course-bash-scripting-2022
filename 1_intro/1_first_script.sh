1ec4219277dad584acde4a52be9cbccdea46e8b6
crrincon <cristian.rincon@mercadolibre.com.co>
2022-02-08 18:06:05 -0500

    :sparkles: first script created

---
+++ 1_intro/1_first_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Program:
+#   This program allows you to perform utilitary operations for PostgreSQL.
+
+echo "This program allows you to perform utilitary operations for PostgreSQL."
