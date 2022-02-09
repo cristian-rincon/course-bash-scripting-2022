b33d79fc79d38213ce9141a52a65171dbb82c223
crrincon <cristian.rincon@mercadolibre.com.co>
2022-02-08 20:15:16 -0500

    :sparkles: download files class

---
+++ 2_interactive_scripts/6_download_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Program:
+#   This program allows you to download files from a remote server.
+# Path: 2_interactive_scripts/6_download_files.sh
+# Author:
+#  Cristian Rincón
+#
+# How to run this script:
+# ./2_interactive_scripts/6_download_files.sh
+
+echo "Downloading files"
+echo "========================="
+
+wget http://www.google.com/robots.txt
