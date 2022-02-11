d216da90598aee766cf8402a0f06f54b3c71bb75
crrincon <cristian.rincon@mercadolibre.com.co>
2022-02-10 19:45:33 -0500

    :sparkles: class 22 - case sentences

--- 3_conditionals/5_case_sentences.sh
+++ 3_conditionals/5_case_sentences.sh
@@ -3,48 +3,26 @@
 # Program:
 #    This is an example of conditional expressions.
 
-age=0
-country=""
-filepath=""
+option=""
 
 
-read -p "Enter your age: " age
-echo -e "\n"
-read -p "Enter your country: " country
-echo -e "\n"
-read -p "Enter your filepath: " filepath
+read -p "Enter your option between A-Z: " option
 echo -e "\n"
 
-echo -e "\n Conditional expressions with numbers: \n"
-
-if [ $age -ge 18 ]; then
-    echo "You are an adult"
-elif [ $age -ge 13 ]; then
-    echo "You are a teenager"
-elif [ $age -ge 7 ]; then
-    echo "You are a child"
-else
-    echo "You are a baby"
-fi
-
-echo -e "\n Conditional expressions with strings: \n"
-
-if [ $country = "India" ]; then
-    echo "You are from India"
-elif [ $country = "USA" ]; then
-    echo "You are from USA"
-elif [ $country = "UK" ]; then
-    echo "You are from UK"
-else
-    echo "You are from somewhere else"
-fi
-
-echo -e "\n Conditional expressions with filepath: \n"
-
-if [ -d $filepath ]; then
-    echo "The filepath is a directory"
-elif [ -f $filepath ]; then
-    echo "The filepath is a file"
-else
-    echo "The filepath is neither a directory nor a file"
-fi
+case $option in
+    A)
+        echo "You selected option A"
+        ;;
+    B)
+        echo "You selected option B"
+        ;;
+    C)
+        echo "You selected option C"
+        ;;
+    D)
+        echo "You selected option D"
+        ;;
+    *)
+        echo "You selected an invalid option"
+        ;;
+esac
